5fdec71d8d2aa0f674997daae2a3e0419ee06318
nisustim <nisustim@gmail.com>
2019-12-26 09:39:19 +0800

    NsTm/: Matplot animate

--- renderer/prac_animate_canvas.py
+++ renderer/prac_animate_canvas.py
@@ -3,7 +3,7 @@ from matplotlib.figure import Figure
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg \
   as FigureCanvas
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore, QtGui
 import numpy as np
 
 # Ensure using PyQt5 backend.
@@ -13,6 +13,9 @@ matplotlib.rcParams["axes.unicode_minus"] = False
 
 # Matplolib canvas class to create figure
 class CanvasRenderer(FigureCanvas):
+  FRAME_PER_SECOND = 10
+  update_time = 0
+  count = 0
   zh_font = matplotlib.font_manager.FontProperties(
     fname=r"/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc")
     # fname=r"/usr/share/fonts-droid-fallback/truetype/DroidSansFallback.ttf")
@@ -35,7 +38,9 @@ class CanvasRenderer(FigureCanvas):
                           QtWidgets.QSizePolicy.Expanding)
     super().updateGeometry()
 
-    self.DrawStaticImage()
+    # self.DrawStaticImage()
+    self.timer = None
+    self.PlotAnimate()
 
   def DrawStaticImage(self):
     # Generate data
@@ -44,6 +49,26 @@ class CanvasRenderer(FigureCanvas):
 
     # Plot
     self.axes.set_xlim(0, 20)
+    self.axes.plot(self.x, self.y, "c")
+
+    self.axes.set_xlabel("時間 time", fontproperties=self.zh_font)
+    self.axes.set_ylabel("振幅 amplitude", fontproperties=self.zh_font)
+    self.axes.grid(True)
+    self.draw()
+
+  def PlotAnimate(self):
+    self.timer = QtCore.QTimer(self)
+    self.timer.timeout.connect(self.UpdateAnimate)
+    self.timer.start(1000 / self.FRAME_PER_SECOND)  # in milli-second
+
+  def UpdateAnimate(self):
+    self.update_time += 1
+    # Generate data
+    self.x = np.linspace(self.update_time, self.update_time + 20, 100)
+    self.y = np.sin(self.x)
+
+    # Plot
+    self.axes.set_xlim(self.update_time, self.update_time + 20)
     self.axes.plot(self.x, self.y, "c")
 
     self.axes.set_xlabel("時間 time", fontproperties=self.zh_font)
